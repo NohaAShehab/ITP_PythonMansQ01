df72db92f927c77d91d9d281013f96bb85fe0b35
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2021-11-01 13:49:48 +0200

    Day03Continue

---
+++ Day03/playground.py
@@ -0,0 +1,39 @@
+# # # m = 1000  # object of int --->  'int' has no len()
+# # # print(len(m))
+# # # import validationmodule  # import all file part
+# # #
+# # # print(validationmodule.validateName("  "))
+# # # print(validationmodule.validateEmail("ggg"))
+# #
+# # # import part from the file
+# # from validationmodule import validateName, validateEmail,x
+# # # print(validateName("   "))
+# # # print(validateEmail("abchhh"))
+# # # print(x)
+# #
+# # # import validationmodule
+# # # print(validationmodule.x)
+# #
+# #
+# # alias
+# # from validationmodule import validateName as myvalidname
+# # from testmodule import validateName
+# # print(myvalidname("ahmed"))
+# # print(validateName("test"))
+# # import validationmodule
+# # import testmodule
+# # validationmodule.validateName("dd")
+# # testmodule.validateName("ffff")
+#
+# from itipackage.greet import helloworld, goodmorning
+#
+# # helloworld("ahmed")
+# #
+# # from itipackage.math_ import add
+# #
+# # print(add(4, 4))
+#
+# import testpackage
+# import itipackage
+# testpackage.abc()
+# testpackage.greetpackege()
